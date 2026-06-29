a43dff2ce9e83003f4a00f52ab3ef595cddafa65
jtwhyte96 <whytejoseph0@gmail.com>
2026-06-29 18:17:58 +0100

    Made a lot of improvements

--- src/practice/TestCode.java
+++ src/practice/TestCode.java
@@ -1,9 +1,13 @@
 package practice;
 
 public class TestCode {
+	
+	static int bean;
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!!");
+		int doe;
+		System.out.println(bean);
 
 	}
 
